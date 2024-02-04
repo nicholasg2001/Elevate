01149def6cc51d59db8c24910e1c404b92b6d52a
kyoshinoda <kyoshisew@gmail.com>
2024-02-03 23:15:31 -0500

    installed react with tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center text-4xl bg-cyan-500 text-white h-screen w-screen">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
