b89695d7528d3c2d7be13fc5a4b972eb65eb7f6b
kyoshinoda <kyoshisew@gmail.com>
2024-02-08 16:11:51 -0500

    added jwt and auth token

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const userRoute = require("./routes/userRoute.js");
+const authRoute = require("./routes/authRoute.js");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 const server = app.listen(3001, () => {
   console.log("listening on port 3001");
