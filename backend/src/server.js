94f2876d6c9ca2221e7a721969ee7ebdbe019d35
kyoshinoda <kyoshisew@gmail.com>
2024-02-09 19:32:58 -0500

    added endpoints to implement populate data

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,19 @@
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const userRoute = require('./routes/userRoute.js');
-const authRoute = require('./routes/authRoute.js');
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const userRoute = require("./routes/userRoute.js");
+const authRoute = require("./routes/authRoute.js");
+const externalApiRoute = require("./routes/externalApiRoute.js");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api/users', userRoute);
-app.use('/api/auth', authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/external", externalApiRoute);
 
 app.listen(3001, () => {
   console.log(`Server is running on port ${3001}`);
