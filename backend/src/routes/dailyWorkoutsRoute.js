e152ddf3ac8029151c4c9589c2291257e53e4d4e
SuperNovaXYZ <alimn2@farmingdale.edu>
2024-02-21 21:04:09 -0500

    Fixed Route :)

--- backend/src/routes/dailyWorkoutsRoute.js
+++ backend/src/routes/dailyWorkoutsRoute.js
@@ -3,7 +3,7 @@ const DailyWorkoutsController = require("../controllers/DailyWorkoutsController"
 const AuthController = require("../controllers/AuthController");
 const router = express.Router();
 
-router.get("/", DailyWorkoutsController.getDailyWorkouts);
+router.get("/", AuthController.authenticateToken, DailyWorkoutsController.getDailyWorkouts);
 router.get("/:date", DailyWorkoutsController.getDailyWorkoutsbyDate);
 
 module.exports = router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const userRoute = require("./routes/userRoute.js");
 const authRoute = require("./routes/authRoute.js");
 const externalApiRoute = require("./routes/externalApiRoute.js");
-
+const dailyWorkoutsRoute = require("./routes/dailyWorkoutsRoute.js");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -14,6 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/external", externalApiRoute);
+app.use("/api/dailyWorkouts", dailyWorkoutsRoute);
+
 
 app.listen(3001, () => {
   console.log(`Server is running on port ${3001}`);
