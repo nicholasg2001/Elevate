ca4665906d72f4a9bcb4d0548ebe7a6114e59764
kyoshinoda <kyoshisew@gmail.com>
2024-03-10 22:01:17 -0400

    WIP added infinite scroll broke search

--- client/src/components/Workouts/WorkoutsContainer.jsx
+++ client/src/components/Workouts/WorkoutsContainer.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Spinner } from "flowbite-react";
-import { useGetAllWorkoutsQuery } from "../../redux/services/WorkoutService";
 import Legs from "/src/assets/workouts/legs.png";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { useInView } from "react-intersection-observer";
@@ -11,8 +10,6 @@ import { fetchWorkoutPage } from "./workoutScrollUtils";
 const WorkoutsContainer = ({ search }) => {
   const [workoutID, setWorkoutID] = useState(0);
 
-  const { data: workout, isLoading } = useGetAllWorkoutsQuery("Workouts");
-
   const {
     data,
     error,
@@ -21,7 +18,7 @@ const WorkoutsContainer = ({ search }) => {
     isFetchingNextPage,
     hasNextPage,
   } = useInfiniteQuery({
-    queryKey: ["items"],
+    queryKey: ["workouts"],
     queryFn: fetchWorkoutPage,
     initialPageParam: 0,
     getNextPageParam: (lastPage) => lastPage.nextPage,
@@ -35,40 +32,26 @@ const WorkoutsContainer = ({ search }) => {
     }
   }, [fetchNextPage, inView, hasNextPage]);
 
-  const filterWorkouts = (workouts) => {
-    return workouts.filter((workout) =>
-      workout.name
-        .toLowerCase()
-        .replace(/[^a-zA-Z]+/g, "")
-        .includes(search.toLowerCase().replace(/[^a-zA-Z]+/g, ""))
-    );
-  };
   return (
     <div className="container mx-auto p-4 lg:p-10 relative">
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-        {isLoading && (
-          <div className="flex justify-center w-screen">
-            <Spinner aria-label="Profile loading spinner" size="xl" />
-          </div>
-        )}
         {data?.pages.map((page) => (
-          <div key={page.currentPage} className="flex flex-col">
-            {page.data.map((workout) => (
-              <WorkoutCard
-                key={workout.workout_id}
-                onClick={() => setWorkoutID(workout.workout_id)}
-                workout={workout}
-                img={Legs}
-              />
-            ))}
-          </div>
+          page.data.map((workout) => (
+            <WorkoutCard
+              key={workout.workout_id}
+              onClick={() => setWorkoutID(workout.workout_id)}
+              workout={workout}
+              img={Legs}
+            />
+          ))
         ))}
+      </div>
+      <div ref={ref}>
         {isFetchingNextPage && (
           <div className="flex justify-center w-screen">
             <Spinner aria-label="Profile loading spinner" size="xl" />
           </div>
         )}
-        <div ref={ref}>{isFetchingNextPage && "Loading..."}</div>
       </div>
       <AddDailyWorkoutModal workout_id={workoutID} />
     </div>
