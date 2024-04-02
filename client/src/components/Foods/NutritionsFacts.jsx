1c1a79ca924d58107985f852d0f86e010735f6f7
kyoshinoda <kyoshisew@gmail.com>
2024-04-02 00:09:28 -0400

    wip

--- client/src/components/Foods/NutritionsFacts.jsx
+++ client/src/components/Foods/NutritionsFacts.jsx
@@ -1,4 +1,4 @@
-const NutritionsFacts = () => {
+const NutritionsFacts = ({ daily, nutritions }) => {
   return (
     <div className="w-1/2 flex flex-col gap-4 shadow-xl bg-white text-slate-700 rounded-xl px-5 py-2">
       <div className="flex flex-col gap-1">
@@ -19,7 +19,7 @@ const NutritionsFacts = () => {
           </span>
           <div className="flex justify-between items-center text-3xl font-bold">
             <h1>Calories</h1>
-            <span>273</span>
+            <span>{Number(nutritions.ENERC_KCAL.quantity).toFixed(0)}</span>
           </div>
           <div className="border-b-4 w-full border-gray-300" />
         </div>
@@ -32,21 +32,38 @@ const NutritionsFacts = () => {
         <div className="flex flex-col gap-2">
           <div className="flex justify-between items-center">
             <h1 className="text-md font-bold">
-              Total Fat <span className="font-normal">13.20g</span>
+              Total Fat
+              <span className="font-normal ml-1">
+                {Number(nutritions.FAT.quantity ?? 0).toFixed(2)}
+                {nutritions.FAT.unit}
+              </span>
             </h1>
-            <span className="font-bold">20%</span>
+            <span className="font-bold">
+              {Number(daily.FAT.quantity ?? 0).toFixed(2)}%
+            </span>
           </div>
           <div className="flex flex-col gap-1 ml-6">
             <div className="border-b w-full border-gray-300" />
             <div className="flex justify-between items-center">
               <h1 className="text-md font-normal">
-                Saturated Fat 2.35g
+                Saturated Fat
+                <span className="ml-1">
+                  {Number(nutritions.FASAT.quantity ?? 0).toFixed(2)}
+                  {nutritions.FASAT.unit}
+                </span>
               </h1>
-              <span className="font-bold">12%</span>
+              <span className="font-bold">
+                {Number(daily.FASAT.quantity ?? 0).toFixed(2)}%
+              </span>
             </div>
             <div className="border-b w-full border-gray-300" />
             <div className="flex justify-between items-center">
-              <h1 className="text-md font-normal">Trans Fat 0.08g</h1>
+              <h1 className="text-md font-normal">Fatty Acids
+                <span className="ml-1">
+                  {Number(nutritions.FAPU.quantity ?? 0).toFixed(2)}
+                  {nutritions.FAPU.unit}
+                </span>
+              </h1>
             </div>
           </div>
           <div className="border-b w-full border-gray-300" />
@@ -54,39 +71,69 @@ const NutritionsFacts = () => {
         <div className="flex flex-col gap-2">
           <div className="flex justify-between items-center">
             <h1 className="text-md font-bold">
-              Cholesterol <span className="font-normal">9.00mg</span>
+              Cholesterol
+              <span className="font-normal ml-1">
+                {Number(nutritions.CHOLE.quantity ?? 0).toFixed(2)}
+                {nutritions.CHOLE.unit}
+              </span>
             </h1>
-            <span className="font-bold">3%</span>
+            <span className="font-bold">
+              {Number(daily.CHOLE.quantity).toFixed(0)}%
+            </span>
           </div>
           <div className="border-b w-full border-gray-300" />
         </div>
         <div className="flex flex-col gap-2">
           <div className="flex justify-between items-center">
             <h1 className="text-md font-bold">
-              Sodium <span className="font-normal">320mg</span>
+              Sodium
+              <span className="font-normal ml-1">
+                {Number(nutritions.NA.quantity ?? 0).toFixed(2)}
+                {nutritions.NA.unit}
+              </span>
             </h1>
-            <span className="font-bold">14%</span>
+            <span className="font-bold">
+              {Number(daily.NA.quantity).toFixed(0)}%
+            </span>
           </div>
           <div className="border-b w-full border-gray-300" />
         </div>
         <div className="flex flex-col gap-2">
           <div className="flex justify-between items-center">
             <h1 className="text-md font-bold">
-              Total Carbohydrate <span className="font-normal">67.74g</span>
+              Total Carbohydrate
+              <span className="font-normal ml-1">
+                {Number(nutritions.CHOCDF.quantity ?? 0).toFixed(2)}
+                {nutritions.CHOCDF.unit}
+              </span>
             </h1>
-            <span className="font-bold">23%</span>
+            <span className="font-bold">
+              {Number(daily.CHOCDF.quantity ?? 0).toFixed(0)}%
+            </span>
           </div>
           <div className="flex flex-col gap-1 ml-6">
             <div className="border-b w-full border-gray-300" />
             <div className="flex justify-between items-center">
               <h1 className="text-md font-normal">
-                Dietary Fiber 0.00g
+                Dietary Fiber
+                <span className="ml-1">
+                  {Number(nutritions.FIBTG.quantity ?? 0).toFixed(2)}
+                  {nutritions.FIBTG.unit}
+                </span>
               </h1>
-              <span className="font-bold">0%</span>
+              <span className="font-bold">
+                {Number(daily.FIBTG.quantity ?? 0).toFixed(0)}%
+              </span>
             </div>
             <div className="border-b w-full border-gray-300" />
             <div className="flex justify-between items-center">
-              <h1 className="text-md font-normal">Total Sugars 61.29g</h1>
+              <h1 className="text-md font-normal">
+                Total Sugars
+                <span className="ml-1">
+                  {Number(nutritions.SUGAR.quantity ?? 0).toFixed(2)}
+                  {nutritions.SUGAR.unit}
+                </span>
+              </h1>
             </div>
             <div className="border-b w-full border-gray-300" />
             <h1 className="text-md font-normal">Includes 0g Added Sugars</h1>
@@ -96,54 +143,90 @@ const NutritionsFacts = () => {
         <div className="flex flex-col gap-2">
           <div className="flex justify-between items-center">
             <h1 className="text-md font-bold">
-              Protein <span className="font-normal">12.90g</span>
+              Protein
+              <span className="font-normal ml-1">
+                {Number(nutritions.PROCNT.quantity ?? 0).toFixed(2)}
+                {nutritions.PROCNT.unit}
+              </span>
             </h1>
-            <span className="font-bold">26%</span>
+            <span className="font-bold">
+              {Number(daily.PROCNT.quantity ?? 0).toFixed(0)}%
+            </span>
           </div>
           <div className="border-b w-full border-gray-300" />
         </div>
         <div className="flex flex-col gap-2">
           <div className="flex justify-between items-center">
             <h1 className="text-md">
-              Vitamin D <span className="font-normal">1.5mcg</span>
+              Vitamin D
+              <span className="font-normal ml-1">
+                {Number(nutritions.VITD.quantity ?? 0).toFixed(2)}
+                {nutritions.VITD.unit}
+              </span>
             </h1>
-            <span className="font-bold">8%</span>
+            <span className="font-bold">
+              {Number(daily.VITD.quantity ?? 0).toFixed(0)}%
+            </span>
           </div>
           <div className="border-b w-full border-gray-300" />
         </div>
         <div className="flex flex-col gap-2">
           <div className="flex justify-between items-center">
             <h1 className="text-md">
-              Calcium <span className="font-normal">250mg</span>
+              Calcium
+              <span className="font-normal ml-1">
+                {Number(nutritions.CA.quantity ?? 0).toFixed(2)}
+                {nutritions.CA.unit}
+              </span>
             </h1>
-            <span className="font-bold">25%</span>
+            <span className="font-bold">
+              {Number(daily.CA.quantity ?? 0).toFixed(0)}%
+            </span>
           </div>
           <div className="border-b w-full border-gray-300" />
         </div>
         <div className="flex flex-col gap-2">
           <div className="flex justify-between items-center">
             <h1 className="text-md">
-              Iron <span className="font-normal">2.5mg</span>
+              Iron
+              <span className="font-normal ml-1">
+                {Number(nutritions.FE.quantity ?? 0).toFixed(2)}
+                {nutritions.FE.unit}
+              </span>
             </h1>
-            <span className="font-bold">14%</span>
+            <span className="font-bold">
+              {Number(daily.FE.quantity ?? 0).toFixed(0)}%
+            </span>
           </div>
           <div className="border-b w-full border-gray-300" />
         </div>
         <div className="flex flex-col gap-2">
           <div className="flex justify-between items-center">
             <h1 className="text-md">
-              Potassium <span className="font-normal">400mg</span>
+              Potassium
+              <span className="font-normal ml-1">
+                {Number(nutritions.K.quantity ?? 0).toFixed(2)}
+                {nutritions.K.unit}
+              </span>
             </h1>
-            <span className="font-bold">10%</span>
+            <span className="font-bold ml-1">
+              {Number(daily.K.quantity ?? 0).toFixed(0)}%
+            </span>
           </div>
           <div className="border-b w-full border-gray-300" />
         </div>
         <div className="flex flex-col gap-2">
           <div className="flex justify-between items-center">
             <h1 className="text-md">
-              Vitamin C <span className="font-normal">30mg</span>
+              Vitamin C
+              <span className="font-normal ml-1">
+                {Number(nutritions.VITC.quantity ?? 0).toFixed(2)}
+                {nutritions.VITC.unit}
+              </span>
             </h1>
-            <span className="font-bold">50%</span>
+            <span className="font-bold">
+              {Number(daily.VITC.quantity ?? 0).toFixed(0)}%
+            </span>
           </div>
           <div className="border-b w-full border-gray-300" />
         </div>
--- client/src/pages/NutritionsPage.jsx
+++ client/src/pages/NutritionsPage.jsx
@@ -1,105 +1,130 @@
 import { useParams } from "react-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import FoodDetailedChart from "../components/Main/Charts/FoodDetailsChart";
 import NutritionsFacts from "../components/Foods/NutritionsFacts";
-
+import { Spinner } from "flowbite-react";
 const NutritionsPage = () => {
   const { foodID } = useParams();
-  //   useEffect(() => {
-  //     const id = import.meta.env.VITE_APP_ID;
-  //     const key = import.meta.env.VITE_APP_KEY;
-  //     // we maybe change this later on with redux
-  //     // we also might need to change the measureURI as well....
-  //     const defaultQuantity = {
-  //       ingredients: [
-  //         {
-  //           quantity: 1,
-  //           measureURI:
-  //             "http://www.edamam.com/ontologies/edamam.owl#Measure_unit",
-  //           foodId: "food_b5klbjta0fg0whahomv72agdt8av",
-  //         },
-  //       ],
-  //     };
-  //     const response = axios
-  //       .post(
-  //         `https://api.edamam.com/api/food-database/v2/nutrients?app_id=${id}&app_key=${key}`,
-  //         defaultQuantity
-  //       )
-  //       .then((res) => {
-  //         console.log(res.data);
-  //       })
-  //       .catch((err) => {
-  //         console.log(err);
-  //       });
-  //   }, []);
-  return (
-    <div className="bg-cyan-500 flex justify-evenly w-screen h-screen p-2">
-      <div className="bg-gray-50 flex flex-col items-center w-5/6 h-full">
-        <div className="flex flex-col gap-5 items-center w-5/6 h-full">
-          <div className="flex w-3/4 items-start gap-10">
-            <div className="w-1/5 h-32 items-center rounded-lg flex justify-center bg-gray-200">
-              image
-            </div>
-            <div className="flex justify-center items-center h-full">
-              <h1 className="text-4xl font-bold item">Cheese Pizza</h1>
-            </div>
-          </div>
-          <div className="border-b w-full border-gray-300" />
+  const [response, setResponse] = useState();
+  useEffect(() => {
+    const id = import.meta.env.VITE_APP_ID;
+    const key = import.meta.env.VITE_APP_KEY;
+    // we maybe change this later on with redux
+    // we also might need to change the measureURI as well....
+    const defaultQuantity = {
+      ingredients: [
+        {
+          quantity: 1,
+          measureURI:
+            "http://www.edamam.com/ontologies/edamam.owl#Measure_unit",
+          foodId: foodID,
+        },
+      ],
+    };
+    axios
+      .post(
+        `https://api.edamam.com/api/food-database/v2/nutrients?app_id=${id}&app_key=${key}`,
+        defaultQuantity
+      )
+      .then((res) => {
+        setResponse(res.data);
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-          <div className="flex justify-evenly w-full gap-6 h-full py-4">
-            <div className="flex flex-col gap-10 w-1/2">
-              <div className="flex justify-around items-center rounded-xl">
-                <div className="flex flex-col items-center justify-center">
-                  <span>Daily Value</span>
-                  <span>21%</span>
+  return (
+    <>
+      {response ? (<div className="bg-cyan-500 flex justify-evenly w-screen h-screen p-2">
+        <div className="bg-gray-50 flex flex-col items-center w-5/6 h-full">
+          <div className="flex flex-col gap-5 items-center w-5/6 h-full">
+            <div className="flex w-3/4 items-start gap-10">
+              <div className="w-1/5 h-32 items-center rounded-lg flex justify-center bg-gray-200">
+                image
+              </div>
+              <div className="flex justify-center items-center h-full">
+                <h1 className="text-4xl font-bold item">{response.ingredients[0].parsed[0].food}</h1>
+              </div>
+            </div>
+            <div className="border-b w-full border-gray-300" />
+            <div className="flex justify-evenly w-full gap-6 h-full py-4">
+              <div className="flex flex-col gap-10 w-1/2">
+                <div className="flex justify-around items-center rounded-xl">
+                  <div className="flex flex-col items-center justify-center text-2xl">
+                    <span className="font-bold ">Calories</span>
+                    <span>{response.calories}</span>
+                  </div>
+                  <div>
+                    <FoodDetailedChart />
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <div className="flex gap-2 items-center">
+                      <div className="w-4 h-4 rounded-full bg-red-500" />
+                      <span className="font-semibold">
+                        {Number((response.totalDaily?.FAT.quantity ?? 0)).toFixed(0)}%
+                      </span>
+                      Fat
+                    </div>
+                    <div className="flex gap-2 items-center">
+                      <div className="w-4 h-4 rounded-full bg-orange-400" />
+                      <span className="font-semibold">
+                        {Number(response.totalDaily?.CHOCDF.quantity ?? 0).toFixed(0)}%
+                      </span>
+                      Carbs
+                    </div>
+                    <div className="flex gap-2 items-center">
+                      <div className="w-4 h-4 rounded-full bg-green-400" />
+                      <span className="font-semibold">
+                        {Number(response.totalDaily?.PROCNT.quantity ?? 0).toFixed(0)}%
+                      </span>
+                      Protein
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-200 p-4 grid grid-cols-3 gap-4 rounded-lg">
+                  <div className="text-center">
+                    <div className="text-gray-600 mb-1 font-bold">Fats</div>
+                    <div className="text-black">
+                      {Number(response.totalNutrients?.FAT.quantity ?? 0).toFixed(2)}g
+                    </div>
+                  </div>
+                  <div className="text-center">
+                    <div className="text-gray-600 mb-1 font-bold">Carbs</div>
+                    <div className="text-black">
+                      {Number(response.totalNutrients?.CHOCDF.quantity ?? 0).toFixed(2)}g
+                    </div>
+                  </div>
+                  <div className="text-center">
+                    <div className="text-gray-600 mb-1 font-bold">Protein</div>
+                    <div className="text-black">
+                      {Number(response.totalNutrients?.PROCNT.quantity ?? 0).toFixed(2)}g
+                    </div>
+                  </div>
                 </div>
                 <div>
-                  <FoodDetailedChart />
-                </div>
-                <div className="flex flex-col gap-2">
-                  <div className="flex gap-2 items-center">
-                    <div class="w-4 h-4 rounded-full bg-red-500" />
-                    <span className="font-semibold">26%</span> Fat
-                  </div>
-                  <div className="flex gap-2 items-center">
-                    <div class="w-4 h-4 rounded-full bg-orange-400" />
-                    <span className="font-semibold">35%</span> Carbs
-                  </div>
-                  <div className="flex gap-2 items-center">
-                    <div className="w-4 h-4 rounded-full bg-green-400" />
-                    <span className="font-semibold">39%</span> Protein
+                  <h1 className="text-3xl font-semibold">Nutrition claims</h1>
+                  <div className="flex gap-1 flex-wrap">
+                    {response.healthLabels.map((label) => (
+                      <div className="text-gray-600">•{label}</div>
+                    ))}
                   </div>
                 </div>
               </div>
-              <div className="bg-gray-200 p-4 grid grid-cols-3 gap-4 rounded-lg">
-                <div className="text-center">
-                  <div className="text-gray-600 mb-1 font-bold">Fats</div>
-                  <div className="text-black">11.29g</div>
-                </div>
-                <div className="text-center">
-                  <div className="text-gray-600 mb-1 font-bold">Carbs</div>
-                  <div className="text-black">67.2g</div>
-                </div>
-                <div className="text-center">
-                  <div className="text-gray-600 mb-1 font-bold">Protein</div>
-                  <div className="text-black">23g</div>
-                </div>
-              </div>
-              <div>
-                <h1 className="text-3xl font-semibold">Nutrition claims</h1>
-                <div className=" flex gap-1">
-                  <span className="text-gray-600 ">•Low Fat abs</span>
-                  <div className="text-gray-600">•Low Fat abs</div>
-                  <div className="text-gray-600">•Low Fat abs</div>
-                </div>
-              </div>
+              <NutritionsFacts
+                daily={response.totalDaily}
+                nutritions={response.totalNutrients}
+              />
             </div>
-            <NutritionsFacts />
           </div>
         </div>
-      </div>
-    </div>
+      </div>) : <div className="flex justify-center w-screen">
+        <Spinner aria-label="Profile loading spinner" size="xl" />
+      </div>}
+    </>
+
   );
 };
 
