322946209303e673a47276b7649229801f5af72b
kyoshinoda <kyoshisew@gmail.com>
2024-04-06 22:16:58 -0400

    added rtk endpoints

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ const authRoute = require("./routes/authRoute.js");
 const externalApiRoute = require("./routes/externalApiRoute.js");
 const dailyWorkoutsRoute = require("./routes/dailyWorkoutsRoute.js");
 const workoutsRoute = require("./routes/workoutsRoute.js");
-const intakesRoute = require("./routes/dailyIntakeRoute.js");
+const dailyFoodsRoute = require("./routes/dailyFoodsRoute.js");
 const videosRoute = require("./routes/videoRoute.js")
 const cloudinary = require("cloudinary").v2;
 
@@ -27,9 +27,9 @@ app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/external", externalApiRoute);
 app.use("/api/dailyWorkouts", dailyWorkoutsRoute);
+app.use("/api/dailyFoods", dailyFoodsRoute);
 app.use("/api/workouts", workoutsRoute);
 app.use("/api/videos", videosRoute)
-app.use("/api/intake", intakesRoute);
 
 
 app.listen(3001, () => {
