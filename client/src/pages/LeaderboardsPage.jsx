46516534987734a6e668261da7b428248f1e5a45
kyoshinoda <kyoshisew@gmail.com>
2024-04-10 23:12:11 -0400

    finished story

--- client/src/pages/LeaderboardsPage.jsx
+++ client/src/pages/LeaderboardsPage.jsx
@@ -1,38 +1,19 @@
-import React, { useState, useEffect } from 'react';
 import { useGetAllUsersQuery } from '../redux/services/UserService';
 import { Spinner } from "flowbite-react";
+import LeaderBoard from './../components/Leaderboards/LeaderBoard';
 
 const LeaderboardsPage = () => {
-
-  const {data, isLoading} = useGetAllUsersQuery("User");
+  const { data, isLoading } = useGetAllUsersQuery("User");
 
   return (
-    <div className="container mx-auto py-8">
-      {isLoading ? (
+    <div className="flex justify-center items-start h-screen w-screen bg-gradient-to-b from-color7 to-color3">
+      {data ? (
+        <div className='w-5/6 flex justify-center flex-col'>
+          <h1 className="text-3xl font-bold text-center mb-3">Leaderboards</h1>
+          <LeaderBoard users={data} />
+        </div>
+      ) : isLoading ? (
         <Spinner aria-label="Default status example" />
-      ) : data.length > 0 ? (
-        <>
-          <h1 className="text-3xl font-bold mb-4">Leaderboards</h1>
-          <div className="grid grid-cols-3 gap-4">
-            {data.map((user) => (
-              <div
-                key={user.user_id}
-                className="border border-gray-200 p-4 rounded shadow-md"
-              >
-                <img
-                  src={user.profileurl}
-                  alt={user.name}
-                  className="w-16 h-16 rounded-full mb-2"
-                />
-                <div className="font-semibold">{user.name}</div>
-                <div className="text-gray-500">{user.email}</div>
-                <div className="text-gray-500">
-                  Height: {user.height} cm, Weight: {user.weight} kg
-                </div>
-              </div>
-            ))}
-          </div>
-        </>
       ) : (
         <p>No users found.</p>
       )}
