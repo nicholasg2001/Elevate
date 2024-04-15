e1943e2b0b0f6d4a15ff8fbc5635625555e639cb
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-04-15 16:49:43 -0400

    Finished map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,22 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from '@react-google-maps/api';
 
 const GymMap = () => {
-  const { isLoaded } = useJsApiLoader({
+
+  const libraries = ['places'];
+  const { isLoaded, loadError } = useJsApiLoader({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_KEY,
+    libraries: libraries,
   });
   const [location, setLocation] = useState('');
   const [gyms, setGyms] = useState([]);
   const [selectedGym, setSelectedGym] = useState(null);
+  const [map, setMap] = useState(null);
+  const [autocomplete, setAutocomplete] = useState(null); 
+
+  useEffect(() => {
+    if (isLoaded) {
+      const mapInstance = new window.google.maps.Map(document.createElement('div'));
+      setMap(mapInstance);
+      // Initialize autocomplete
+      const autocompleteInstance = new window.google.maps.places.Autocomplete(
+        document.getElementById('locationInput')
+      );
+      setAutocomplete(autocompleteInstance);
+    }
+  }, [isLoaded]);
 
   const handleSearch = async (e) => {
     e.preventDefault();
     try {
-      const geocoder = new google.maps.Geocoder();
+      const geocoder = new window.google.maps.Geocoder();
       geocoder.geocode({ address: location }, (results, status) => {
-        if (status === google.maps.GeocoderStatus.OK) {
+        if (status === window.google.maps.GeocoderStatus.OK) {
           const { lat, lng } = results[0].geometry.location;
-          const placesService = new google.maps.places.PlacesService(map);
+          const placesService = new window.google.maps.places.PlacesService(map);
           placesService.nearbySearch(
             {
               location: { lat: lat(), lng: lng() },
@@ -24,7 +41,7 @@ const GymMap = () => {
               type: ['gym'],
             },
             (results, status) => {
-              if (status === google.maps.places.PlacesServiceStatus.OK) {
+              if (status === window.google.maps.places.PlacesServiceStatus.OK) {
                 setGyms(results);
               }
             }
@@ -36,23 +53,53 @@ const GymMap = () => {
     }
   };
 
-  return (
-    <div>
-      <form onSubmit={handleSearch}>
-        <input
-          type="text"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-          placeholder="Enter a location"
-        />
-        <button type="submit">Search</button>
-      </form>
+  const handleClear = () => {
+    setGyms([]);
+    setLocation(''); 
+    setSelectedGym(null); 
+  };
 
+
+  useEffect(() => {
+    if (autocomplete) {
+      // Add event listener for place changed event to update location state
+      autocomplete.addListener('place_changed', () => {
+        const place = autocomplete.getPlace();
+        if (!place.geometry || !place.geometry.location) {
+          console.error('Autocomplete returned place with no geometry');
+          return;
+        }
+        setLocation(place.formatted_address); // Update location state with formatted address
+      });
+    }
+  }, [autocomplete]);
+
+  if (loadError) return 'Error loading maps';
+
+  return (
+    <div className="flex flex-row mt-5">
+      <div className="flex flex-col mr-2">
+        <form onSubmit={handleSearch}>
+          <button className="mr-2"type="submit">🔍</button>
+          <input
+            id="locationInput"
+            type="text"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            placeholder="Enter a location"
+          />
+        </form>
+        {location && (
+          <button className="font-poppins rounded-xl text-white mt-2 bg-red-500"onClick={handleClear}>
+            Clear
+          </button>
+        )}
+      </div>
       {isLoaded && (
         <GoogleMap
           center={{ lat: 40.78, lng: -73.15 }}
           zoom={10}
-          mapContainerStyle={{ height: '500px', width: '100%' }}
+          mapContainerStyle={{ flex: 1, height: '550px', width: '100%' }}
         >
           {gyms.map((gym) => (
             <Marker
@@ -66,6 +113,12 @@ const GymMap = () => {
               {selectedGym === gym && (
                 <InfoWindow onCloseClick={() => setSelectedGym(null)}>
                   <div>
+                  {gym.photos && gym.photos.length > 0 && (
+                    <img
+                      src={gym.photos[0].getUrl({ maxWidth: 200, maxHeight: 100 })}
+                      alt={gym.name}
+                    />
+                  )}
                     <h3>{gym.name}</h3>
                     <p>{gym.vicinity}</p>
                   </div>
--- client/src/pages/MapsPage.jsx
+++ client/src/pages/MapsPage.jsx
@@ -1,13 +1,11 @@
-import Map from "../components/Map";
-
-
+import GymMap from "/src/components/Map";
 
 const MapsPage = () => {
   return (
     <div className="flex justify-center items-center h-screen w-screen bg-gradient-to-b from-color7 to-color3">
         <div className="h-5/6 w-5/6 flex justify-center items-center bg-white rounded-2xl">
-            <div className="w-5/6">
-                <Map />
+            <div className="w-full px-3">
+                <GymMap />
             </div>
         </div>
     </div>
