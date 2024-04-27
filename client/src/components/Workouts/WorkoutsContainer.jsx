ed712a66652402d025659f049b2225adbbc3b0cf
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-04-27 13:38:22 -0400

    Filter images based on muscle group

--- client/src/components/Workouts/WorkoutsContainer.jsx
+++ client/src/components/Workouts/WorkoutsContainer.jsx
@@ -1,6 +1,18 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Spinner } from "flowbite-react";
-import Legs from "/src/assets/workouts/legs.png";
+import Abductors from "/src/assets/muscles/Abductors.jpg";
+import Adductors from "/src/assets/muscles/Adductors.jpg";
+import Abs from "/src/assets/muscles/Abs.jpg";
+import Back from "/src/assets/muscles/Back.jpg";
+import Biceps from "/src/assets/muscles/Biceps.jpg";
+import Calves from "/src/assets/muscles/Calves.jpg";
+import Chest from "/src/assets/muscles/Chest.jpg";
+import Forearms from "/src/assets/muscles/Forearms.jpg";
+import Hamstrings from "/src/assets/muscles/Hamstrings.jpg";
+import Traps from "/src/assets/muscles/Traps.jpg";
+import Triceps from "/src/assets/muscles/Triceps.jpeg";
+import Quadriceps from "/src/assets/muscles/Quadriceps.jpeg";
+import Shoulders from "/src/assets/muscles/Shoulders.jpeg";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { useInView } from "react-intersection-observer";
 import WorkoutCard from "./WorkoutCard";
@@ -30,14 +42,64 @@ const WorkoutsContainer = ({ search }) => {
     }
   }, [fetchNextPage, inView, hasNextPage]);
   
-  const filterWorkouts = (workouts) => {
-    return workouts.filter((workout) =>
-      workout.name
-        .toLowerCase()
-        .replace(/[^a-zA-Z]+/g, "")
-        .includes(search.toLowerCase().replace(/[^a-zA-Z]+/g, ""))
-    );
+  const filterWorkoutImages = (muscle) => {
+    let img;
+    switch (muscle.toLowerCase()) {
+      case "abdominals":
+        img = Abs;
+        break;
+      case "abductors":
+        img = Abductors;
+        break;
+      case "adductors":
+        img = Adductors;
+        break;
+      case "biceps":
+        img = Biceps;
+        break;
+      case "calves":
+        img = Calves;
+        break;
+      case "chest":
+        img = Chest;
+        break;
+      case "forearms":
+        img = Forearms;
+        break;
+      case "glutes":
+        img = Abductors; 
+        break;
+      case "hamstrings":
+        img = Hamstrings;
+        break;
+      case "lats":
+        img = Back;
+        break;
+      case "lower_back":
+        img = Back;
+        break;
+      case "middle_back":
+        img = Back;
+        break;
+      case "neck":
+        img = Back;
+        break;
+      case "traps":
+        img = Traps;
+        break;
+      case "quadriceps":
+        img = Quadriceps;
+        break;
+      case "triceps":
+        img = Triceps;
+        break;
+      case "shoulders":
+        img = Shoulders;
+        break;
+    }
+    return img;
   };
+  
 
   return (
     <div className="container mx-auto p-4 lg:p-10 relative">
@@ -48,7 +110,7 @@ const WorkoutsContainer = ({ search }) => {
               key={workout.workout_id}
               onClick={() => setWorkoutID(workout.workout_id)}
               workout={workout}
-              img={Legs}
+              img={filterWorkoutImages(workout.muscle)}
             />
           ))
         )}
