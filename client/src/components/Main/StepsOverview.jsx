5fb995f4f57f91c5c42cc0d96afa812d66687596
kyoshinoda <kyoshisew@gmail.com>
2024-04-29 22:20:54 -0400

    added basic overview

---
+++ client/src/components/Main/StepsOverview.jsx
@@ -0,0 +1,24 @@
+import ProgressBar from "@ramonak/react-progress-bar";
+import { GiConverseShoe } from "react-icons/gi";
+const StepsOverview = () => {
+  return (
+    <div className="w-1/3 h-full bg-white rounded-xl relative">
+      <div className="absolute top-1 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-purple-300  rounded-full p-2 z-10">
+        <GiConverseShoe size={40} className="text-white" />
+      </div>
+      <div className="pt-12 pb-4 px-4">
+        <div className="flex flex-col text-center mt-6">
+          <span className="font-bold text-purple-700 text-xl">Daily Steps</span>
+          <span className="text-gray-500">10 km / week</span>
+        </div>
+        <div className="my-2">
+          Progress
+          <ProgressBar completed={95} />
+        </div>
+        <span className="text-gray-500 font-semibold">Target: 50km</span>
+      </div>
+    </div>
+  )
+}
+
+export default StepsOverview;
--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -7,6 +7,12 @@ import WeeklyStepChart from "../components/Main/Charts/WeeklyStepsChart";
 import WeeklyWorkoutsChart from "../components/Main/Charts/WeeklyWorkoutsChart";
 import CustomToasts from "../components/Toasts/CustomToasts";
 import { GiConverseShoe } from "react-icons/gi";
+import { FaBottleWater } from "react-icons/fa6";
+import StepsOverview from "../components/Main/StepsOverview";
+import BikeOverview from "../components/Main/BikeOverview";
+import RunningOverview from "../components/Main/RunningOverview";
+
+
 const MainPage = () => {
   const { user } = useAppSelector((state) => state.auth);
   const dispatch = useAppDispatch();
@@ -18,23 +24,42 @@ const MainPage = () => {
   }, []);
   return (
     <>
-      <div className="flex justify-center bg-cyan-500 dark:bg-slate-800 w-screen h-screen">
-        <div className="flex flex-col h-full w-3/4 bg-white rounded-xl justify-center">
-          <div className="flex gap-4 w-full h-3/4">
-            <div className="w-1/2 flex justify-center items-center">
+      <div className="flex justify-center bg-gradient-to-b from-color7 to-color3 dark:from-darkColor1 dark:to-darkColor2 w-screen h-screen">
+        <div className="flex flex-col h-full w-3/4 bg-gray-200 rounded-xl justify-center px-10 gap-10">
+          <h1 className="text-5xl font-bold text-center">Weekly Report</h1>
+          <div className="flex gap-14 w-full h-1/2">
+            <div className="w-3/4 flex justify-center items-center bg-white rounded-lg">
               <WeeklyStepChart />
             </div>
-            <div className="flex flex-col w-1/2 gap-10">
+            <div className="flex flex-col w-1/4 gap-10">
               <div className="bg-gradient-to-t from-blue-800 via-blue-500 to-cyan-500 h-1/6 cursor-pointer rounded-xl flex justify-center items-center gap-4">
                 <div className="bg-white rounded-xl flex justify-center items-center h-1/2 w-1/4">
                   <GiConverseShoe size={30} />
                 </div>
                 <span className="text-lg text-white font-bold">
-                    Daily Walking
+                  Daily Walking
                 </span>
               </div>
-              <div className="bg-cyan-200 rounded h-3/4">xxx</div>
+              <div className="bg-cyan-600 rounded-xl h-3/4 p-1">
+                <div className="bg-cyan-500 h-1/3 rounded-xl flex justify-center gap-4 items-center">
+                  <div className="bg-blue-400 h-14 w-14 rounded-lg flex justify-center items-center">
+                    <FaBottleWater size={30} color="white" />
+                  </div>
+                  <span className="text-lg text-white font-bold">
+                    Water Intake
+                  </span>
+                </div>
+                <div className="flex flex-col gap-1 text-white">
+                  <span>Total Ounces</span>
+                  70
+                </div>
+              </div>
             </div>
+          </div>
+          <div className="flex gap-10 h-1/4">
+            <BikeOverview />
+            <RunningOverview />
+            <StepsOverview />
           </div>
         </div>
       </div>
